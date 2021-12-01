046846a7bc2f5378de65e451ad5e41e2090d3568
Amir79Naziri <naziriamirreza@gmail.com>
2021-12-02 03:25:23 +0330

    "a little change"

--- js/script.js
+++ js/script.js
@@ -10,81 +10,94 @@ const error_message = document.getElementById("error-message")
 
 let last_name_from_storage = null;
 
-function validate_search_name() {
-    let name = search_text.value
+// function validate_search_name() {
+//     let name = search_text.value
+//
+//     if (name.length === 0) {
+//         throw new Error("please enter your name")
+//     } else if (name.length > 255) {
+//         throw new Error("name is more than 255 character")
+//     } else
+//         return name
+// }
+//
+// function which_gender_chosen() {
+//     let radio_btn = document.getElementsByName("gender-btn")
+//     for (let i = 0; i < radio_btn.length; i++) {
+//         if (radio_btn[i].checked) return radio_btn[i].value
+//     }
+// }
+//
+// async function save_name_on_storage(name, gender) {
+//
+//     window.localStorage.setItem(name, gender)
+// }
+//
+// async function load_name_from_storage() {
+//
+//     let name = validate_search_name()
+//     let gender = window.localStorage.getItem(name)
+//     console.log(`${name} loaded from storage, gender is ${gender}`)
+//     return gender
+// }
+//
+// async function remove_name_from_storage(name) {
+//     if (name != null) {
+//         window.localStorage.removeItem(name)
+//         console.log(`${name} removed from storage`)
+//     }
+//     saved_gender.innerText = ""
+// }
+//
+// async function get_data_from_server(name) {
+//
+//     let url = `https://api.genderize.io/?name=${name}`
+//
+//     let response = await fetch(url)
+//
+//     if (!response.ok) {
+//         throw new Error('some thing went wrong at server')
+//     }
+//
+//     let json = await response.json()
+// }
+//
+//
+//
+// function clear_clicked() {
+//     if (last_name_from_storage != null) {
+//         remove_name_from_storage(last_name_from_storage).then(free_save_card)
+//     }
+// }
+//
+// function save_clicked() {
+//
+// }
+//
+// function free_save_card() {
+//     saved_gender.style.visibility = "hidden"
+// }
+//
+// function fill_result_card(gender, p) {
+//     result_gender.innerText = gender
+//     result_percentage.innerText = p
+//     result_gender.style.visibility = "visible"
+//     result_percentage.style.visibility ="visible"
+// }
+//
+// function fill_save_card(gender) {
+//     saved_gender.innerText = gender
+//     saved_gender.style.visibility = "visible"
+// }
 
-    if (name.length === 0) {
-        throw new Error("please enter your name")
-    } else if (name.length > 255) {
-        throw new Error("name is more than 255 character")
-    } else
-        return name
-}
-
-
-function which_gender_chosen() {
-    let radio_btn = document.getElementsByName("gender-btn")
-    for (let i = 0; i < radio_btn.length; i++) {
-        if (radio_btn[i].checked) return radio_btn[i].value
-    }
-}
-
-function save_name_on_storage(name, gender) {
-
-    window.localStorage.setItem(name, gender)
-}
-
-function load_name_from_storage() {
-
-    let name = validate_search_name()
-    let gender = window.localStorage.getItem(name)
-    console.log(`${name} loaded from storage, gender is ${gender}`)
-    return gender
-}
-
-
-function remove_name_from_storage(name) {
-    if (name != null) {
-        window.localStorage.removeItem(name)
-        console.log(`${name} removed from storage`)
-    }
-    saved_gender.innerText = ""
-}
-
-async function get_data_from_server(name) {
-
-    let url = `https://api.genderize.io/?name=${name}`
-
-    let response = await fetch(url)
-
-    if (!response.ok) {
-        throw new Error('some thing went wrong at server')
-    }
-
-    let json = await response.json()
-}
 
 async function submit_clicked() {
+    let response = await fetch("https://api.genderize.io/?name=hassan");
 
+    let data = await response.json()
+
+    console.log(JSON.stringify(data))
 }
-
-async function save_clicked() {
-    try {
-        let name = validate_search_name()
-        let gender = which_gender_chosen()
-
-        save_name_on_storage(name, gender)
-    } catch (e) {
-        console.log(e.message)
-    }
-}
-
-async function clear_clicked() {
-    if(last_name_from_storage != null) {
-        remove_name_from_storage(last_name_from_storage)
-    }
-}
-
 
 submit_btn.addEventListener('click', submit_clicked)
 save_btn.addEventListener('click', save_clicked)
